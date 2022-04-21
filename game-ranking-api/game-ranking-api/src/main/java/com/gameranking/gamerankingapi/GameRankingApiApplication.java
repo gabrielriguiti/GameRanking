218d15c09c1e6af7e94bda5aae8552df28a61920
gabrielriguiti <1768grO@>
2022-04-20 23:10:15 -0400

    Configuração do projeto backend

---
+++ game-ranking-api/game-ranking-api/src/main/java/com/gameranking/gamerankingapi/GameRankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.gameranking.gamerankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRankingApiApplication.class, args);
+    }
+
+}
---
+++ game-ranking-api/game-ranking-api/src/test/java/com/gameranking/gamerankingapi/GameRankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameranking.gamerankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
