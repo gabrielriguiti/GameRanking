a5cbb2f25797654e3966e3a571495ad4531ecc65
gabrielriguiti <1768grO@>
2022-04-22 08:28:22 -0400

    Habilitando CORS

--- game-ranking-api/src/main/java/com/gameranking/GameRankingApiApplication.java
+++ game-ranking-api/src/main/java/com/gameranking/GameRankingApiApplication.java
@@ -2,12 +2,18 @@ package com.gameranking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class GameRankingApiApplication {
+public class GameRankingApiApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GameRankingApiApplication.class, args);
     }
-
 }
